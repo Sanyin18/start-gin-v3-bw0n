44807b6e9b072f23780cca3b5937b096b61ffcb2
Sanyin18 <125891523+Sanyin18@users.noreply.github.com>
2024-07-15 23:55:45 +0800

    Update main.go

--- code/main.go
+++ code/main.go
@@ -13,7 +13,7 @@ func main() {
 	})
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "hello world",
+			"message": "hello world1",
 		})
 	})
 	// listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
